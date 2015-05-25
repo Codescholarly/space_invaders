8881d3eaaab4dbdbbefe4d419d1bf11efa274780
Manuel Fernandez <manfergo25@gmail.com>
2015-05-25 12:54:56 -0430

    Joystick update9

--- js/game.js
+++ js/game.js
@@ -118,7 +118,7 @@ var Game = {
 		{
 	       game.input.touch.touchStartCallback = this.onTouchStart;
 	       game.input.touch.touchMoveCallback = this.onTouchMove;
-	       game.input.touch.touchLeaveCallback = this.onTouchLeave;	
+	//       game.input.touch.touchLeaveCallback = this.onTouchLeave;	
 		}
 //		else
 //		{
